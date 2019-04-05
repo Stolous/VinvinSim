7e8a79c99e385ccdfd08f00bf42d929b616990de
Stolous <stolous@gmail.com>
2019-04-05 23:22:16 +0200

    Added 2 axis keyboard control

---
+++ Assets/Scripts/VinvinController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VinvinController : MonoBehaviour
+{
+
+    public WheelCollider leftWheel, rightWheel;
+    public float maxMotorTorque;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftWheel.steerAngle = 90f;
+        rightWheel.steerAngle = 90f;
+
+    }
+
+    void FixedUpdate()
+    {
+        float leftTorque = maxMotorTorque * Input.GetAxis("Vertical");
+        float rightTorque = maxMotorTorque * Input.GetAxis("Horizontal");
+
+        leftWheel.motorTorque = leftTorque;
+        rightWheel.motorTorque = rightTorque;
+    }
+}
