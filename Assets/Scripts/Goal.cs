312b295d0fe40dbba921539ce9fe45bfae3ad491
Baptiste Véré <stolous@gmail.com>
2019-04-26 13:46:40 +0200

    Added ball and goals

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public Transform initialBallTransform;
+    public string opponentTeamName;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.transform.position = this.initialBallTransform.position;
+        Debug.Log(this.opponentTeamName + " scored !");
+    }
+}
