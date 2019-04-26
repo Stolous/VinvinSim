091c270b23af6591642ba7c9c723975cd952b33f
Baptiste Véré <stolous@gmail.com>
2019-04-26 14:25:18 +0200

    Fixed goals trigger collision

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -10,6 +10,8 @@ public class Goal : MonoBehaviour
     public void OnTriggerEnter(Collider other)
     {
         other.gameObject.transform.position = this.initialBallTransform.position;
+        other.gameObject.GetComponent<Rigidbody>().inertiaTensor = Vector3.zero;
+        other.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
         Debug.Log(this.opponentTeamName + " scored !");
     }
 }
